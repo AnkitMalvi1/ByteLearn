8f5586d62f606d4298ef50828571ab4b3c57381b
AnkitMalvi1 <ankitmalviya06219@gmail.com>
2025-02-04 15:54:37 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+const App = () => {
+  return (
+    <div>
+      Unknown at rule @tailwind css(unknownAtRules)
+
+      Stack Overflow
+      https://stackoverflow.com › questions › unknown-at-rul...
+      Use the files.associations setting to tell VS Code to always open .css files in Tailwind CSS mode
+      9 answers
+
+      ·
+
+      Top answer:
+      You could look at installing the Tailwind CSS IntelliSense VSCode plugin and then: files.associatio ...
+      Unknown at rule @tailwind CSS in reactjs - Stack Overflow
+      5 Jan 2022
+      Laravel Unknown at rule @tailwindcss(unknownAtRules)
+      22 Feb 2022
+      How to add a @tailwind CSS rule to CSS checker
+      2 Dec 2017
+      reactjs - Tailwind CSS VSCode Warning - Unknown at rule ...
+      26 Oct 2024
+      More results from stackoverflow.com
+    </div>
+  )
+}
+
+export default App
